4d16f7f4deb4edb14ec9c1e7b18ebacc18167d48
fntstcflX <janflorianjacob@icloud.com>
2020-07-21 12:21:45 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// Event Listeners
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+
+// Functions
+function addTodo(event) {
+  // Prevent Form from submitting
+  event.preventDefault();
+  // TODO DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  // CREATE LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  // CECKMARK BUTTON
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  // TRASH BUTTON
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  // APPEND TO LIST
+  todoList.appendChild(todoDiv);
+  // CLEAS TO DO INPUT
+  todoInput.value = "";
+}
+
+function deleteCheck(event) {
+  const item = e.target;
+  // DELETE TODO
+}
